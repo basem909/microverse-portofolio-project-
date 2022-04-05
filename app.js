18c52e5167535fbdb4a81585fc03b41281bdb3b6
Bassem <bassem@MacBook-Pro-de-Bassem.local>
2022-04-05 02:13:28 -0300

    add event listner to the links so it closes down the menu when clicked

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
-const hamburgerIcon = document.querySelector(".image");
-const navbar = document.querySelector(".navigator");
-const upper = document.querySelector(".upper");
+const hamburgerIcon = document.querySelector('.image');
+const navbar = document.querySelector('.navigator');
+const upper = document.querySelector('.upper');
+const links = document.querySelectorAll('.nav-link');
+hamburgerIcon.addEventListener('click', () => {
+  navbar.classList.toggle('menu-open');
+  upper.classList.toggle('upper-menu-open');
+  hamburgerIcon.classList.toggle('image-menu-open');
+  upper.classList.toggle('link-menu-open');
+});
 
-hamburgerIcon.addEventListener("click", () => {
-  navbar.classList.toggle("menu-open");
-  upper.classList.toggle("upper-menu-open");
-  hamburgerIcon.classList.toggle("image-menu-open");
+links.addEventListener('click', () => {
+  navbar.classList.remove('menu-open');
+  upper.classList.remove('upper-menu-open');
+  hamburgerIcon.classList.remove('image-menu-open');
+  upper.classList.remove('link-menu-open');
 });
