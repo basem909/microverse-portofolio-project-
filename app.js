edee0faf00533d6bc53eb275696f8459c720eedb
Bassem <bassem@MacBook-Pro-de-Bassem.local>
2022-04-05 03:12:24 -0300

    fix style linter errors

--- app.js
+++ app.js
@@ -1,26 +1,30 @@
-const hamburgerIcon = document.querySelector(".image");
-const xIcon = document.querySelector(".image2");
-const navbar = document.querySelector(".navigator");
-const upper = document.querySelector(".upper");
-const links = document.querySelectorAll(".nav-link");
-hamburgerIcon.addEventListener("click", () => {
-  navbar.classList.toggle("menu-open");
-  upper.classList.toggle("upper-menu-open");
-  hamburgerIcon.classList.toggle("image-menu-open");
-  upper.classList.toggle("link-menu-open");
-  xIcon.classList.toggle("image-menu-appear");
+const hamburgerIcon = document.querySelector('.image');
+const xIcon = document.querySelector('.image2');
+const navbar = document.querySelector('.navigator');
+const upper = document.querySelector('.upper');
+const linkMenu = document.querySelectorAll('.navigator > a');
+
+hamburgerIcon.addEventListener('click', () => {
+  navbar.classList.toggle('menu-open');
+  upper.classList.toggle('upper-menu-open');
+  hamburgerIcon.classList.toggle('image-menu-open');
+  upper.classList.toggle('link-menu-open');
+  xIcon.classList.toggle('image-menu-appear');
 });
 
-xIcon.addEventListener("click", () => {
-  navbar.classList.remove("menu-open");
-  upper.classList.remove("upper-menu-open");
-  hamburgerIcon.classList.remove("image-menu-open");
-  upper.classList.remove("link-menu-open");
-  xIcon.classList.toggle("image-menu-appear");
+
+xIcon.addEventListener('click', () => {
+  navbar.classList.remove('menu-open');
+  upper.classList.remove('upper-menu-open');
+  hamburgerIcon.classList.remove('image-menu-open');
+  upper.classList.remove('link-menu-open');
+  xIcon.classList.remove('image-menu-appear');
 });
-links.addEventListener("click", () => {
-  navbar.classList.remove("menu-open");
-  upper.classList.remove("upper-menu-open");
-  hamburgerIcon.classList.remove("image-menu-open");
-  upper.classList.remove("link-menu-open");
+
+linkMenu.addEventListener('click', () => {
+  navbar.classList.remove('menu-open');
+  upper.classList.remove('upper-menu-open');
+  hamburgerIcon.classList.remove('image-menu-open');
+  upper.classList.remove('link-menu-open');
+  xIcon.classList.remove('image-menu-appear');
 });
