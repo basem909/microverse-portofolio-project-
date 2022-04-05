1c6c46667bfc8f6eb28809db943ae034200a133b
Bassem <bassem@MacBook-Pro-de-Bassem.local>
2022-04-05 02:42:13 -0300

    add an X icon to the open status of the hamburger menu

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
-const hamburgerIcon = document.querySelector('.image');
-const navbar = document.querySelector('.navigator');
-const upper = document.querySelector('.upper');
-const links = document.querySelectorAll('.nav-link');
-hamburgerIcon.addEventListener('click', () => {
-  navbar.classList.toggle('menu-open');
-  upper.classList.toggle('upper-menu-open');
-  hamburgerIcon.classList.toggle('image-menu-open');
-  upper.classList.toggle('link-menu-open');
+const hamburgerIcon = document.querySelector(".image");
+const xIcon = document.querySelector(".image2");
+const navbar = document.querySelector(".navigator");
+const upper = document.querySelector(".upper");
+const links = document.querySelectorAll(".nav-link");
+hamburgerIcon.addEventListener("click", () => {
+  navbar.classList.toggle("menu-open");
+  upper.classList.toggle("upper-menu-open");
+  hamburgerIcon.classList.toggle("image-menu-open");
+  upper.classList.toggle("link-menu-open");
+  xIcon.classList.toggle("image-menu-appear");
 });
 
-links.addEventListener('click', () => {
-  navbar.classList.remove('menu-open');
-  upper.classList.remove('upper-menu-open');
-  hamburgerIcon.classList.remove('image-menu-open');
-  upper.classList.remove('link-menu-open');
+xIcon.addEventListener("click", () => {
+  navbar.classList.remove("menu-open");
+  upper.classList.remove("upper-menu-open");
+  hamburgerIcon.classList.remove("image-menu-open");
+  upper.classList.remove("link-menu-open");
+  xIcon.classList.toggle("image-menu-appear");
+});
+links.addEventListener("click", () => {
+  navbar.classList.remove("menu-open");
+  upper.classList.remove("upper-menu-open");
+  hamburgerIcon.classList.remove("image-menu-open");
+  upper.classList.remove("link-menu-open");
 });
