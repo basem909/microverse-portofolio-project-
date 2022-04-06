4177f9cd4dc678dc2a1f12fee1a561b6e200d5c4
Bassem <bassem@MacBook-Pro-de-Bassem.local>
2022-04-06 17:10:07 -0300

    fix javascript linter problems

--- app.js
+++ app.js
@@ -1,38 +1,37 @@
-const hamburgerIcon = document.querySelector(".image");
-const xIcon = document.querySelector(".image2");
-const navbar = document.querySelector(".navigator");
-const upper = document.querySelector(".upper");
-const linkMenu = document.querySelectorAll(".nav-link");
+const hamburgerIcon = document.querySelector('.image');
+const xIcon = document.querySelector('.image2');
+const navbar = document.querySelector('.navigator');
+const upper = document.querySelector('.upper');
+const linkMenu = document.querySelectorAll('.nav-link');
 
-hamburgerIcon.addEventListener("click", () => {
-  navbar.classList.toggle("menu-open");
-  upper.classList.toggle("upper-menu-open");
-  hamburgerIcon.classList.toggle("image-menu-open");
-  upper.classList.toggle("link-menu-open");
-  xIcon.classList.toggle("image-menu-appear");
+hamburgerIcon.addEventListener('click', () => {
+  navbar.classList.toggle('menu-open');
+  upper.classList.toggle('upper-menu-open');
+  hamburgerIcon.classList.toggle('image-menu-open');
+  upper.classList.toggle('link-menu-open');
+  xIcon.classList.toggle('image-menu-appear');
 });
 
 const closeMenu = () => {
-  navbar.classList.remove("menu-open");
-  upper.classList.remove("upper-menu-open");
-  hamburgerIcon.classList.remove("image-menu-open");
-  upper.classList.remove("link-menu-open");
-  xIcon.classList.remove("image-menu-appear");
+  navbar.classList.remove('menu-open');
+  upper.classList.remove('upper-menu-open');
+  hamburgerIcon.classList.remove('image-menu-open');
+  upper.classList.remove('link-menu-open');
+  xIcon.classList.remove('image-menu-appear');
 };
-xIcon.addEventListener("click", closeMenu);
+xIcon.addEventListener('click', closeMenu);
 
 linkMenu.forEach((link) => {
-  link.addEventListener("click", closeMenu);
+  link.addEventListener('click', closeMenu);
 });
 
-const email = document.getElementById("mail");
-const form = document.getElementById("contact-form");
-const messageError = document.getElementById("error");
+const email = document.getElementById('mail');
+const form = document.getElementById('contact-form');
+const messageError = document.getElementById('error');
 
-form.addEventListener("submit", (e) => {
-  if (email.value != email.value.toLowerCase()) {
+form.addEventListener('submit', (e) => {
+  if (email.value !== email.value.toLowerCase()) {
     e.preventDefault();
-    messageError.innerText =
-      "unfortunatlly the message was not submitted, The email address needs to be all small characters";
+    messageError.innerText = 'unfortunatlly the message was not submitted, The email address needs to be all small characters';
   }
 });
