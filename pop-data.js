4e447ee0d08ae2af93990b7a87896e3b48bb1138
Bassem <bassem@MacBook-Pro-de-Bassem.local>
2022-04-07 19:20:12 -0300

    create pop-data.js file and create the array of objects to create the pop up window

---
+++ pop-data.js
@@ -0,0 +1,58 @@
+const popUp = [
+  {
+    Title: "project name goes here",
+    Technologies: ["HTML/CSS", "Ruby on rails", "Javascript"],
+    Image: "assests/images/Rectangle21.png",
+    Description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque repudiandae tempora nisi minima non beatae aspernatur praesentium ipsa? Quia quidem debitis error tenetur, ea eos facere natus tempor neque ducimus doloribus assumenda aspernatur iusto soluta enim ataccusantium impedit ratione quasi perferendis cum hic esse ipsum sit!<br/><br/>br Atque corrupti unde impedit commodi voluptate cum inventore tenetur sequi accusantium fugiat consectetur provident voluptatem blanditiis quae excepturi repudiandae sunt rem ipsam, magni voluptas ullam ab nulla vero exercitationem! Tempora, consequatur velit! Hic aut explicabo voluptates sed ipsum eos praesentium a inventore fuga mollitia nostrum, ducimus incidunt! Quos quo in rerum blanditiis perferendis!",
+    LinkLive: "https://basem909.github.io/portfolio/",
+    LinkSource: "https://github.com/",
+  },
+  {
+    Title: "project name goes here",
+    Technologies: ["HTML/CSS", "Ruby on rails", "Javascript"],
+    Image: "assests/images/Rectangle27.png",
+    Description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque repudiandae tempora nisi minima non beatae aspernatur praesentium ipsa? Quia quidem debitis error tenetur, ea eos facere natus tempor neque ducimus doloribus assumenda aspernatur iusto soluta enim ataccusantium impedit ratione quasi perferendis cum hic esse ipsum sit!<br/><br/>br Atque corrupti unde impedit commodi voluptate cum inventore tenetur sequi accusantium fugiat consectetur provident voluptatem blanditiis quae excepturi repudiandae sunt rem ipsam, magni voluptas ullam ab nulla vero exercitationem! Tempora, consequatur velit! Hic aut explicabo voluptates sed ipsum eos praesentium a inventore fuga mollitia nostrum, ducimus incidunt! Quos quo in rerum blanditiis perferendis!",
+    LinkLive: "https://basem909.github.io/portfolio/",
+    LinkSource: "https://github.com/",
+  },
+  {
+    Title: "project name goes here",
+    Technologies: ["HTML/CSS", "Ruby on rails", "Javascript"],
+    Image: "assests/images/Rectangle211.png",
+    Description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque repudiandae tempora nisi minima non beatae aspernatur praesentium ipsa? Quia quidem debitis error tenetur, ea eos facere natus tempor neque ducimus doloribus assumenda aspernatur iusto soluta enim ataccusantium impedit ratione quasi perferendis cum hic esse ipsum sit!<br/><br/>br Atque corrupti unde impedit commodi voluptate cum inventore tenetur sequi accusantium fugiat consectetur provident voluptatem blanditiis quae excepturi repudiandae sunt rem ipsam, magni voluptas ullam ab nulla vero exercitationem! Tempora, consequatur velit! Hic aut explicabo voluptates sed ipsum eos praesentium a inventore fuga mollitia nostrum, ducimus incidunt! Quos quo in rerum blanditiis perferendis!",
+    LinkLive: "https://basem909.github.io/portfolio/",
+    LinkSource: "https://github.com/",
+  },
+  {
+    Title: "project name goes here",
+    Technologies: ["HTML/CSS", "Ruby on rails", "Javascript"],
+    Image: "assests/images/Rectangle213.png",
+    Description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque repudiandae tempora nisi minima non beatae aspernatur praesentium ipsa? Quia quidem debitis error tenetur, ea eos facere natus tempor neque ducimus doloribus assumenda aspernatur iusto soluta enim ataccusantium impedit ratione quasi perferendis cum hic esse ipsum sit!<br/><br/>br Atque corrupti unde impedit commodi voluptate cum inventore tenetur sequi accusantium fugiat consectetur provident voluptatem blanditiis quae excepturi repudiandae sunt rem ipsam, magni voluptas ullam ab nulla vero exercitationem! Tempora, consequatur velit! Hic aut explicabo voluptates sed ipsum eos praesentium a inventore fuga mollitia nostrum, ducimus incidunt! Quos quo in rerum blanditiis perferendis!",
+    LinkLive: "https://basem909.github.io/portfolio/",
+    LinkSource: "https://github.com/",
+  },
+  {
+    Title: "project name goes here",
+    Technologies: ["HTML/CSS", "Ruby on rails", "Javascript"],
+    Image: "assests/images/Rectangle214.png",
+    Description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque repudiandae tempora nisi minima non beatae aspernatur praesentium ipsa? Quia quidem debitis error tenetur, ea eos facere natus tempor neque ducimus doloribus assumenda aspernatur iusto soluta enim ataccusantium impedit ratione quasi perferendis cum hic esse ipsum sit!<br/><br/>br Atque corrupti unde impedit commodi voluptate cum inventore tenetur sequi accusantium fugiat consectetur provident voluptatem blanditiis quae excepturi repudiandae sunt rem ipsam, magni voluptas ullam ab nulla vero exercitationem! Tempora, consequatur velit! Hic aut explicabo voluptates sed ipsum eos praesentium a inventore fuga mollitia nostrum, ducimus incidunt! Quos quo in rerum blanditiis perferendis!",
+    LinkLive: "https://basem909.github.io/portfolio/",
+    LinkSource: "https://github.com/",
+  },
+  {
+    Title: "project name goes here",
+    Technologies: ["HTML/CSS", "Ruby on rails", "Javascript"],
+    Image: "assests/images/Rectangle215.png",
+    Description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque repudiandae tempora nisi minima non beatae aspernatur praesentium ipsa? Quia quidem debitis error tenetur, ea eos facere natus tempor neque ducimus doloribus assumenda aspernatur iusto soluta enim ataccusantium impedit ratione quasi perferendis cum hic esse ipsum sit!<br/><br/>br Atque corrupti unde impedit commodi voluptate cum inventore tenetur sequi accusantium fugiat consectetur provident voluptatem blanditiis quae excepturi repudiandae sunt rem ipsam, magni voluptas ullam ab nulla vero exercitationem! Tempora, consequatur velit! Hic aut explicabo voluptates sed ipsum eos praesentium a inventore fuga mollitia nostrum, ducimus incidunt! Quos quo in rerum blanditiis perferendis!",
+    LinkLive: "https://basem909.github.io/portfolio/",
+    LinkSource: "https://github.com/",
+  },
+];
+
+export default popUp;
