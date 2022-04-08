335bf77ea4e1adb55ad33de0c1bb04f3fba5e7c6
Bassem <bassem@MacBook-Pro-de-Bassem.local>
2022-04-08 05:26:49 -0300

    set the layout and the style of the dynamic project card

--- app.js
+++ app.js
@@ -66,3 +66,153 @@ function showLocal(obj, key) {
 showLocal(Name, 'name');
 showLocal(email, 'email');
 showLocal(msg, 'message');
+
+//array of project objects
+const popUp = [
+  {
+    Title: "project name goes here",
+    Technologies: ["HTML/CSS", "Ruby on rails", "Javascript"],
+    Image: "assests/images/Rectangle21.png",
+    Description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque repudiandae tempora nisi minima non beatae aspernatur praesentium ipsa? Quia quidem debitis error tenetur, ea eos facere natus tempor neque ducimus doloribus assumenda aspernatur iusto soluta enim ataccusantium impedit ratione quasi perferendis cum hic esse ipsum sit!<br/><br/>br Atque corrupti unde impedit commodi voluptate cum inventore tenetur sequi accusantium fugiat consectetur provident voluptatem blanditiis quae excepturi repudiandae sunt rem ipsam, magni voluptas ullam ab nulla vero exercitationem! Tempora, consequatur velit! Hic aut explicabo voluptates sed ipsum eos praesentium a inventore fuga mollitia nostrum, ducimus incidunt! Quos quo in rerum blanditiis perferendis!",
+    LinkLive: "https://basem909.github.io/portfolio/",
+    LinkSource: "https://github.com/",
+  },
+  {
+    Title: "project name goes here",
+    Technologies: ["HTML/CSS", "Ruby on rails", "Javascript"],
+    Image: "assests/images/Rectangle27.png",
+    Description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque repudiandae tempora nisi minima non beatae aspernatur praesentium ipsa? Quia quidem debitis error tenetur, ea eos facere natus tempor neque ducimus doloribus assumenda aspernatur iusto soluta enim ataccusantium impedit ratione quasi perferendis cum hic esse ipsum sit!<br/><br/>br Atque corrupti unde impedit commodi voluptate cum inventore tenetur sequi accusantium fugiat consectetur provident voluptatem blanditiis quae excepturi repudiandae sunt rem ipsam, magni voluptas ullam ab nulla vero exercitationem! Tempora, consequatur velit! Hic aut explicabo voluptates sed ipsum eos praesentium a inventore fuga mollitia nostrum, ducimus incidunt! Quos quo in rerum blanditiis perferendis!",
+    LinkLive: "https://basem909.github.io/portfolio/",
+    LinkSource: "https://github.com/",
+  },
+  {
+    Title: "project name goes here",
+    Technologies: ["HTML/CSS", "Ruby on rails", "Javascript"],
+    Image: "assests/images/Rectangle211.png",
+    Description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque repudiandae tempora nisi minima non beatae aspernatur praesentium ipsa? Quia quidem debitis error tenetur, ea eos facere natus tempor neque ducimus doloribus assumenda aspernatur iusto soluta enim ataccusantium impedit ratione quasi perferendis cum hic esse ipsum sit!<br/><br/>br Atque corrupti unde impedit commodi voluptate cum inventore tenetur sequi accusantium fugiat consectetur provident voluptatem blanditiis quae excepturi repudiandae sunt rem ipsam, magni voluptas ullam ab nulla vero exercitationem! Tempora, consequatur velit! Hic aut explicabo voluptates sed ipsum eos praesentium a inventore fuga mollitia nostrum, ducimus incidunt! Quos quo in rerum blanditiis perferendis!",
+    LinkLive: "https://basem909.github.io/portfolio/",
+    LinkSource: "https://github.com/",
+  },
+  {
+    Title: "project name goes here",
+    Technologies: ["HTML/CSS", "Ruby on rails", "Javascript"],
+    Image: "assests/images/Rectangle213.png",
+    Description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque repudiandae tempora nisi minima non beatae aspernatur praesentium ipsa? Quia quidem debitis error tenetur, ea eos facere natus tempor neque ducimus doloribus assumenda aspernatur iusto soluta enim ataccusantium impedit ratione quasi perferendis cum hic esse ipsum sit!<br/><br/>br Atque corrupti unde impedit commodi voluptate cum inventore tenetur sequi accusantium fugiat consectetur provident voluptatem blanditiis quae excepturi repudiandae sunt rem ipsam, magni voluptas ullam ab nulla vero exercitationem! Tempora, consequatur velit! Hic aut explicabo voluptates sed ipsum eos praesentium a inventore fuga mollitia nostrum, ducimus incidunt! Quos quo in rerum blanditiis perferendis!",
+    LinkLive: "https://basem909.github.io/portfolio/",
+    LinkSource: "https://github.com/",
+  },
+  {
+    Title: "project name goes here",
+    Technologies: ["HTML/CSS", "Ruby on rails", "Javascript"],
+    Image: "assests/images/Rectangle214.png",
+    Description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque repudiandae tempora nisi minima non beatae aspernatur praesentium ipsa? Quia quidem debitis error tenetur, ea eos facere natus tempor neque ducimus doloribus assumenda aspernatur iusto soluta enim ataccusantium impedit ratione quasi perferendis cum hic esse ipsum sit!<br/><br/>br Atque corrupti unde impedit commodi voluptate cum inventore tenetur sequi accusantium fugiat consectetur provident voluptatem blanditiis quae excepturi repudiandae sunt rem ipsam, magni voluptas ullam ab nulla vero exercitationem! Tempora, consequatur velit! Hic aut explicabo voluptates sed ipsum eos praesentium a inventore fuga mollitia nostrum, ducimus incidunt! Quos quo in rerum blanditiis perferendis!",
+    LinkLive: "https://basem909.github.io/portfolio/",
+    LinkSource: "https://github.com/",
+  },
+  {
+    Title: "project name goes here",
+    Technologies: ["HTML/CSS", "Ruby on rails", "Javascript"],
+    Image: "assests/images/Rectangle215.png",
+    Description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque repudiandae tempora nisi minima non beatae aspernatur praesentium ipsa? Quia quidem debitis error tenetur, ea eos facere natus tempor neque ducimus doloribus assumenda aspernatur iusto soluta enim ataccusantium impedit ratione quasi perferendis cum hic esse ipsum sit!<br/><br/>br Atque corrupti unde impedit commodi voluptate cum inventore tenetur sequi accusantium fugiat consectetur provident voluptatem blanditiis quae excepturi repudiandae sunt rem ipsam, magni voluptas ullam ab nulla vero exercitationem! Tempora, consequatur velit! Hic aut explicabo voluptates sed ipsum eos praesentium a inventore fuga mollitia nostrum, ducimus incidunt! Quos quo in rerum blanditiis perferendis!",
+    LinkLive: "https://basem909.github.io/portfolio/",
+    LinkSource: "https://github.com/",
+  },
+];
+
+// the project card layout
+
+const container = document.querySelector('.pop-container');
+const header1 = document.createElement('div');
+const xButton = document.createElement('button');
+const xImage = document.createElement('img');
+xButton.appendChild(xImage);
+header1.appendChild(xButton);
+container.appendChild(header1);
+xImage.setAttribute('src','assests/images/Union.svg')
+const head = document.createElement('h3')
+head.textContent = 'Project name goes here'
+container.appendChild(head)
+const list = document.createElement('ul')
+const li1 = document.createElement('li')
+const li2 = document.createElement("li");
+const li3 = document.createElement("li");
+list.appendChild(li1)
+list.appendChild(li2);
+list.appendChild(li3)
+container.appendChild(list)
+li1.textContent='HTML/CSS'
+li2.textContent = "Rubi on rails";
+li3.textContent = "Javascript";
+const imgContainer = document.createElement('div')
+const imgBig = document.createElement('img')
+const  imgSmall1 = document.createElement('img')
+const imgSmall2 = document.createElement("img");
+const imgSmall3 = document.createElement("img");
+const imgSmall4 = document.createElement("img");
+imgBig.setAttribute('src', 'assests/images/Rectangle21.png');
+imgSmall1.setAttribute("src", "assests/images/Rectangle21.png");
+imgSmall2.setAttribute("src", "assests/images/Rectangle21.png");
+imgSmall3.setAttribute("src", "assests/images/Rectangle21.png");
+imgSmall4.setAttribute("src", "assests/images/Rectangle21.png");
+imgContainer.appendChild(imgBig);
+imgContainer.appendChild(imgSmall1);
+imgContainer.appendChild(imgSmall2);
+imgContainer.appendChild(imgSmall3);
+imgContainer.appendChild(imgSmall4);
+container.appendChild(imgContainer)
+const popParagraph = document.createElement('p')
+popParagraph.textContent = 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque repudiandae tempora nisi minima non beatae aspernatur praesentium ipsa? Quia quidem debitis error tenetur, ea eos facere natus tempor neque ducimus doloribus assumenda aspernatur iusto soluta enim ataccusantium impedit ratione quasi perferendis cum hic esse ipsum sit! Atque corrupti unde impedit commodi voluptate cum inventore tenetur sequi accusantium fugiat consectetur provident voluptatem blanditiis quae excepturi repudiandae sunt rem ipsam, magni voluptas ullam ab nulla vero exercitationem! Tempora, consequatur velit! Hic aut explicabo voluptates sed ipsum eos praesentium a inventore fuga mollitia nostrum, ducimus incidunt! Quos quo in rerum blanditiis perferendis!'
+container.appendChild(popParagraph)
+const btnBox = document.createElement('div')
+const liveBtn = document.createElement('button')
+const sourceBtn = document.createElement('button')
+liveBtn.textContent = 'See live'
+sourceBtn.textContent = 'See source'
+btnBox.appendChild(liveBtn)
+btnBox.appendChild(sourceBtn)
+container.appendChild(btnBox)
+const footer = document.createElement('footer')
+const seeNext = document.createElement('a')
+const nextLink = document.createElement('img')
+nextLink.setAttribute("src", "assests/images/arrow-right.svg");
+seeNext.textContent = 'Next project'
+seeNext.appendChild(nextLink)
+seeNext.setAttribute('href',"#")
+const seePrevious = document.createElement('a')
+const previousLink = document.createElement('img')
+seePrevious.textContent = 'Previous project'
+previousLink.setAttribute("src", "assests/images/arrow-left.svg");
+seePrevious.appendChild(previousLink);
+seePrevious.style.flexDirection = 'row-reverse'
+seePrevious.setAttribute('href','#')
+footer.appendChild(seePrevious)
+footer.appendChild(seeNext)
+container.appendChild(footer)
+container.classList.toggle('pop-project1')
+header1.classList.toggle('pop-header')
+xButton.classList.toggle('pop-close')
+head.classList.toggle('pop-heading1')
+list.classList.toggle('pop-ulist1')
+li1.classList.toggle('pop-li1')
+li2.classList.toggle('pop-li1');
+li3.classList.toggle('pop-li1');
+imgContainer.classList.toggle('pop-image1')
+imgBig.classList.toggle('pop-big')
+imgSmall1.classList.toggle('pop-small')
+imgSmall2.classList.toggle('pop-small');
+imgSmall3.classList.toggle('pop-small');
+imgSmall4.classList.toggle('pop-small');
+popParagraph.classList.toggle('pop-paragraph1');
+btnBox.classList.toggle('btn-box')
+liveBtn.classList.toggle('pop-button')
+sourceBtn.classList.toggle('pop-button')
+sourceBtn.setAttribute("id", "pop-button2");
+footer.classList.toggle('footer-pop')
+seeNext.classList.toggle('link-pop')
+seePrevious.classList.toggle('link-pop')
